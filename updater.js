66f377dcb406f36da12daa1af304f1c78ae2cdb5
lsaether <lsaether@protonmail.com>
2017-08-02 12:53:47 -0700

    updater

--- updater.js
+++ updater.js
@@ -1,32 +1,17 @@
-const events = require('events')
+const EventEmitter = require('events').EventEmitter
 
-const update = (interval) => {
-    this.time = time
-    this.array = [
-        {number: 1},
-        {number: 2}
-    ]
-    let that
-    events.EventEmitter.call(this)
-
-    this.init = function() {
-        that = this
-        console.log("Contructor")
-        //Start interval
-        setInterval(that.run,that.time)
+class Updater extends EventEmitter {
+    constructor(interval) {
+        super()
+        this.interval = interval
     }
 
-    this.run = function()
-    {
-        that.array.forEach(function (item) {
-           if(item.number === 2)
-           {
-               that.emit('Event');
-           }
-        })
+    init() {
+        console.log('Updater started.')
+        console.log('-'.repeat(25))
+        setInterval(() => this.emit("Event"), this.interval)
     }
+
 }
 
-update.prototype.__proto__ = events.EventEmitter.prototype
-
-module.exports = update
+module.exports = Updater
